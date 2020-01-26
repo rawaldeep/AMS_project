ef5fa067846aa9caec4a5c6a5e3b7a7f3850b065
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2020-01-26 19:29:39 +0100

    added a signout, placement is subject to change

--- AMSproject/src/screens/MapScreen.js
+++ AMSproject/src/screens/MapScreen.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import { Button, View, Text, StyleSheet, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import firebase from 'firebase';
 
 export default class Mapscreen extends Component {
     render() {
         return (
             <View style={styles.container}>
                 <Text>Map Screen</Text>
+                <Button
+                    title='Sign Out'
+                    onPress={() => firebase.auth().signOut()}
+                />
             </View>
         )
     }
